3e44d253abd94e45797612f7b8b25504c5a80e9d
sahil kacchi <sahilkacchi84@gmail.com>
2020-12-10 10:22:30 +0530

    first commit

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+#this program is about madlib
+
+name = input("name:")
+college = input("college:")
+activity1 = input("activity1:")
+activity2 = input("activity2:")
+
+para = f"hello people my name is {name} i am student of college {college}.\and i love to {activity1} and {activity2}.yess so lets get started....... "
+
+print(para)
